caf252063294749f009aa5ea9f1664b4b080d8b2
Asthar <astharuniacc@gmail.com>
2026-08-27 02:10:03 +0700

    another fix

--- entities/movable_spikes/Vector2_Float.gd
+++
@@ -1,8 +0,0 @@
-class_name VectorFloat
-extends Resource
-
-@export var position: Vector2 = Vector2(0,0)
-@export var seconds: float = 3.0
-
-#func _ready() -> void:
-	#resource_local_to_scene = true
