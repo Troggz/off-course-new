2f7e59d19c300e5350c8f3958f465402d25b8f43
Asthar <astharuniacc@gmail.com>
2026-08-27 00:00:31 +0700

    fixing merge issues

--- entities/player/player.gd
+++ entities/player/player.gd
@@ -87,7 +87,7 @@ func _process(delta: float) -> void:
 		$Sprites/Lupin.scale = Vector2(1.45, 1.45)
 
 	clear_arcs()
-	if not dead:
+	if latchable:
 		draw_trajectories()
 		draw_arcs()
 
@@ -105,7 +105,7 @@ func _process(delta: float) -> void:
 			$Sprites/LatchSmoke.emitting = true
 			$Sprites/Front.modulate = Color(1.0, 0.0, 0.0)
 			$Sprites/Back.modulate = Color(1.0, 0.0, 0.0)
-		elif latched and not latching:
+		if Input.is_action_just_released("latch"):
 			var boost := get_unlatch_boost()
 			#print("boost ", boost)
 			latched = false
